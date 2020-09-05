dff81dfd7d26abbc8b040528e13231f8867f1eee
Ketan Sethi <ketansethi786@gmail.com>
2020-09-05 21:10:42 +0530

    Adding systeminfo in ExtentManager.java and fixing TestNG_Parallel.xml

--- src/main/java/com/automationpractice/reports/ExtentManager.java
+++ src/main/java/com/automationpractice/reports/ExtentManager.java
@@ -1,5 +1,6 @@
 package com.automationpractice.reports;
 
+import com.automationpractice.utilities.InitConfiguration;
 import com.relevantcodes.extentreports.ExtentReports;
 
 import java.io.File;
@@ -9,11 +10,13 @@ public class ExtentManager {
     static String filePath = null;
 
     public synchronized static ExtentReports getReporter() {
+        InitConfiguration conf = new InitConfiguration();
         filePath = System.getProperty("user.dir") + File.separator + "target" + File.separator + "ExtentReports" + File.separator + "Extent.html";
         if (extent == null) {
             extent = new ExtentReports(filePath, true);
         }
         extent.assignProject("Automation Practice");
+        extent.addSystemInfo(conf.getAllConfigurations());
         return extent;
     }
 }
