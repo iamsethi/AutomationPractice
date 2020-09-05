61e5036fed91a74d731de7c5dbea907e55ded53f
Ketan Sethi <ketansethi786@gmail.com>
2020-09-05 21:02:41 +0530

    Adding ExtenReports

---
+++ src/main/java/com/automationpractice/reports/ExtentManager.java
@@ -0,0 +1,19 @@
+package com.automationpractice.reports;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
+import java.io.File;
+
+public class ExtentManager {
+    static ExtentReports extent;
+    static String filePath = null;
+
+    public synchronized static ExtentReports getReporter() {
+        filePath = System.getProperty("user.dir") + File.separator + "target" + File.separator + "ExtentReports" + File.separator + "Extent.html";
+        if (extent == null) {
+            extent = new ExtentReports(filePath, true);
+        }
+        extent.assignProject("Automation Practice");
+        return extent;
+    }
+}
