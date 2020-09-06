84792b8547367f0c8632ab5965eb075411a9e029
Ketan Sethi <ketansethi786@gmail.com>
2020-09-06 13:24:33 +0530

    Method level has highest precedence then Class level then properties level is the lowest

--- src/test/java/com/automationpractice/tests/MyFirstTest.java
+++ src/test/java/com/automationpractice/tests/MyFirstTest.java
@@ -1,14 +1,12 @@
 package com.automationpractice.tests;
 
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+//@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class MyFirstTest extends BaseTest {
 
     @Test
-  //  @DisplayName("Display name has highest precedence")
+        //  @DisplayName("Display name has highest precedence")
     void this_is_my_first_selenium_test() {
         driver.get("http://localhost:8099/");
 
